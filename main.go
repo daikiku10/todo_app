cce7cedfec870135fe254bcda33a7413ab4d716b
daikiku10 <soccerdk19.910@gmail.com>
2022-05-31 08:57:33 +0900

    add: サーバーの立ち上げ

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"todo_app/app/controllers"
 	"todo_app/app/models"
 	// "todo_app/config"
 )
@@ -9,61 +10,5 @@ import (
 func main() {
 	fmt.Println(models.Db)
 
-	/*
-	u := &models.User{}
-	u.Name = "test2"
-	u.Email = "test2@example.com"
-	u.PassWord = "test2test2"
-	fmt.Println(u)
-
-	u.CreateUser()
-	*/
-
-	// u, _ := models.GetUser(1)
-
-	// fmt.Println(u)
-
-	// u.Name = "kudo"
-	// u.Email = "kudo@example.com"
-	// u.UpdateUser()
-	// u, _ = models.GetUser(1)
-	// fmt.Println(u)
-
-	// u.DeleteUser()
-
-	// u, _ = models.GetUser(1)
-	// fmt.Println(u)
-
-	// user, _ := models.GetUser(2)
-	// user.CreateTodo("First Todo")
-
-	// t, _ := models.GetTodo(1)
-	// fmt.Println(t)
-
-	/*
-	user, _ := models.GetUser(3)
-	user.CreateTodo("Third Todo")
-	*/
-
-	/*
-
-		todos, _ := models.GetTodos()
-
-		for _, v := range todos {
-			fmt.Println(v)
-		}
-	*/
-
-	/*
-	user2, _ := models.GetUser(3)
-	todos, _ := user2.GetTodosByUser()
-	for _, v := range todos {
-		fmt.Println(v)
-	}
-	*/
-
-	t, _ := models.GetTodo(3)
-	t.DeleteTodo()
-
-
+	controllers.StartMainServer()
 }
